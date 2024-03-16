60685ab9cd1111bb9edd7ae09a391f470f37720e
eldarlebedevdeveloper <eldarlebedevdeveloper@gmail.com>
2024-03-16 16:38:13 +0200

    upgrade

--- src/App.js
+++ src/App.js
@@ -1,8 +1,40 @@
-import logo from './logo.svg'
-import './App.css'
+import Header from './Header'
+import Nav from './Nav'
+import Footer from './Footer'
+import Home from './Home'
+import NewPost from './NewPost'
+import About from './About'
+import Missing from './Missing'
+import PostPage from './PostPage'
+import { Route, Routes, useHistory } from 'react-router-dom'
+import { useState, useEffect } from 'react'
 
 function App() {
-  return <div className="App">fadfsfsdf</div>
+  return (
+    <div className="App">
+      <Header title="React JS Blog" />
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/post" element={<NewPost />} />
+        <Route path="/post/:id" element={<PostPage />} />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<Missing />} />
+        {/* <Route path="/">
+          <Home />
+        </Route>
+        <Route path="/post">
+          <NewPost />
+        </Route>
+        <Route path="/post/:id">
+          <PostPage />
+        </Route>
+        <Route path="/about" Component={About} />
+        <Route path="*" Component={Missing} /> */}
+      </Routes>
+      <Footer />
+    </div>
+  )
 }
 
 export default App
---
+++ src/Missing.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Missing = () => {
+  return (
+    <main>
+      <h1>missing</h1>
+    </main>
+  )
+}
+export default Missing
